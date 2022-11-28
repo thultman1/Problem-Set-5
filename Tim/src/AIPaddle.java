06f86c17c3e7505d87a237b2e0fb60aaa3f494ac
thultman1 <hultts22@wfu.edu>
2022-11-28 17:49:34 -0500

    AIPaddle template

--- Tim/src/AIPaddle.java
+++ Tim/src/AIPaddle.java
@@ -2,22 +2,24 @@
 import java.awt.*;
 import java.awt.event.*;
 public class AIPaddle extends Rectangle{
-
+    int id;
     int yVelocity;
     int speed = 10;
-    AIPaddle(int x, int y, int PADDLE_WIDTH, int PADDLE_HEIGHT){
+
+    AIPaddle(int x, int y, int PADDLE_WIDTH, int PADDLE_HEIGHT, int id){
         super(x,y,PADDLE_WIDTH,PADDLE_HEIGHT);
+        this.id=id;
     }
 
-    public void setYDirection(int yDirection) {
-        yVelocity = yDirection;
-    }
+
 
     public void move() {
-        y= y + yVelocity;
+       // y=Ball.y;
     }
-
     public void draw(Graphics g) {
+        if(id==1)
+            g.setColor(Color.blue);
+        else
             g.setColor(Color.red);
         g.fillRect(x, y, width, height);
     }
